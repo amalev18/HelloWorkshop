319f8031fc0732d4c9351939cfaad98de0c47eb6
amalev18 <amanda.levinsson@telia.com>
2020-11-06 16:49:47 +0100

    Changed

--- src/HelloWorkshop.java
+++ src/HelloWorkshop.java
@@ -2,6 +2,7 @@ public class HelloWorkshop {
 public static void main(String[] args){
     System.out.println("Hello Amanda");
     System.out.println("Hello there");
+    System.out.println("Hello Amanda");
 
     }
 
